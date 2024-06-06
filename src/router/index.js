7d22452e1898736e840d5822223ddf0f5fd3a038
95941 <959419991@qq.com>
2024-06-06 21:38:33 +0800

    The landing logic has been comprehensively modified. The game landing logic is roughly as follows
    Logging into the game
    
    Enter id, name
    
    login, jump to the login game screen
    
    rule, rule reading
    
    Logging into the game
    
    Player name, select a room, confirm button, Join
    
    If the room is 4 people, you cannot join. The modified game screen shows that the room is full
    
    Logging into the game
    
    Enter id, name
    
    login, jump to the login game screen
    
    rule, rule reading
    
    Logging into the game
    
    Player name, select a room, confirm button, Join
    
    If the room is 4 people, you cannot join. The room is full

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import GameTable from '../views/GameTable.vue'
-import Login from '../views/Login.vue'
 import Menu from '../views/Menu.vue'
 import GameRules from "../views/GameRules.vue";
 import Matching from "../views/Matching.vue";
@@ -9,7 +8,6 @@ const routes = [
     {path:"/", redirect: "/Menu"},
     {path:"/Menu", name: Menu, component: Menu},
     {path:"/GameTable", name: GameTable, component: GameTable},
-    {path:"/Login", name: Login, component: Login},
     {path:"/GameRules", name: GameRules, component: GameRules},
     {path:"/Matching", name: Matching, component: Matching}
 ]
